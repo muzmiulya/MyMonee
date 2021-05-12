47398c8dfe7e2f881986bcb28539a941e90a8afe
Muhammad Muzmi' Ulya <muhammadmuzmiulya@gmail.com>
2021-05-12 08:32:44 +0700

    Initial Commit

---
+++ MyMonee - Money Saver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMonee - Money Saver
+//
+//  Created by MacBook on 12/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
